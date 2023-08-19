ec358dde9ea11189316c56c1a7eeede86d1afe61
Alvin <alvin.sahayam@softsuave.com>
2023-08-19 16:02:10 +0530

    Last Commit

--- db.config.js
+++ db.config.js
@@ -2,8 +2,8 @@ import AWS from 'aws-sdk'
 
 AWS.config.update({
     region: "ap-south-1",
-    accessKeyId: "AKIA2Q2EQLX6VULJTE7V",
-    secretAccessKey: "+iA7/DV6IfqP9ndMn9DXi9Rpl6Mb4pVTMB5mFp8S"
+    accessKeyId: "EXAMPLEKEY",
+    secretAccessKey: "EXAMPLESECRETKEY"
 })
 
 const db = new AWS.DynamoDB.DocumentClient()
